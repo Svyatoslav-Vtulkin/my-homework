b4ec9c65cca11e7878bb08f317a4d72ed81c56cd
PreSicvl <vss200601@gmail.com>
2024-12-02 12:58:20 +0500

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,5 @@
+The_number_of_completed_DZ = 12
+Number_of_hours_spent = 1.5
+Course_name = "Python"
+Time_for_one_task = 1.5/12
+print ("Курс:",Course_name,",","всего задач:",The_number_of_completed_DZ,",","затрачено часов:",Number_of_hours_spent,",","среднее время выполнения:",Time_for_one_task)
