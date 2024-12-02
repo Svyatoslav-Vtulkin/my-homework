a072e1be9c158c366791ca7f853ef6a5a4ceb0f4
PreSicvl <vss200601@gmail.com>
2024-12-02 12:35:41 +0500

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = "Svyatoslav"
+print ("Name:", name)
+age = 18
+print ("Age:", age)
+new_age = age #я не понял как перезаписать в age текущее значение переменной age + новое
+print ("New_age:", new_age) #я подумал что так, но врядли это правильно
+is_student = True
+print ("Is Student:", is_student)
