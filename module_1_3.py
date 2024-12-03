dc120e27bd6bf47fe0a5b982141adaf4be2b48bc
PreSicvl <vss200601@gmail.com>
2024-12-03 16:21:14 +0500

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -2,7 +2,7 @@ name = "Svyatoslav"
 print ("Name:", name)
 age = 18
 print ("Age:", age)
-new_age = age #я не понял как перезаписать в age текущее значение переменной age + новое
+new_age = age + 1 #я не понял как перезаписать в age текущее значение переменной age + новое
 print ("New_age:", new_age) #я подумал что так, но врядли это правильно
 is_student = True
 print ("Is Student:", is_student)
